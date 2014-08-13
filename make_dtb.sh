54f1e2f5493ad5973f6c2f82f6df6bf230d55080
chuM <chuM@163.com>
2014-08-13 13:31:05 +0800

    open debug_ll config suport

--- make_dtb.sh
+++ make_dtb.sh
@@ -3,7 +3,11 @@
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- dtbs
 echo "cp arch/arm/boot/at91sam9g25ek.dtb /opt/tftpboot/at91sam9g25ek.dtb"
 cp arch/arm/boot/at91sam9g25ek.dtb /opt/tftpboot/at91sam9g25ek.dtb
-echo "cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/images"
-cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/images
+echo "cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/weishui_master_images"
+cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/weishui_master_images
+
+
+echo "cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/rfid_comm_manager_images"
+cp arch/arm/boot/at91sam9g25ek.dtb /mnt/hgfs/windown-linux/board_9g25/rfid_comm_manager_images
 
 
--- make_uImage.sh
+++ make_uImage.sh
@@ -4,7 +4,11 @@ cp .config config_now
 make -j2 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- uImage
 echo "cp arch/arm/boot/uImage /opt/tftpboot/uImage"
 cp arch/arm/boot/uImage /opt/tftpboot/uImage
-echo "cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/"
-cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/
+echo "cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/weishui_master_images"
+cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/weishui_master_images
+
+echo "cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/rfid_comm_manager_images"
+cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/rfid_comm_manager_images
+
 
 
